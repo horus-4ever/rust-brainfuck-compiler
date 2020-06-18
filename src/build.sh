975bdf22ae790c44e683775d4809e8c127c783b1
horus-4ever <th.pichard54@gmail.com>
2020-06-18 14:49:45 +0200

    Updated build script

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,7 @@
+if [ ! -d $1/build ]; then
+    mkdir $1/build
+fi
+
 nasm -f elf temp.asm -o $1/build/brainfuck.o
 ld -m elf_i386 -s $1/build/brainfuck.o -o $1/build/brainfuck
 rm $1/build/brainfuck.o $1/temp.asm
