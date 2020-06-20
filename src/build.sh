f92d0cf8d8d913aaecae64d6f6ff63538be8ef61
horus-4ever <th.pichard54@gmail.com>
2020-06-20 21:38:13 +0200

    use gcc instead of ld (integration of the C library

--- src/build.sh
+++ src/build.sh
@@ -2,6 +2,6 @@ if [ ! -d $1/build ]; then
     mkdir $1/build
 fi
 
-nasm -f elf temp.asm -o $1/build/brainfuck.o
-ld -m elf_i386 -s $1/build/brainfuck.o -o $1/build/brainfuck
+nasm -f elf32 $1/temp.asm -o $1/build/brainfuck.o
+gcc -m32 $1/build/brainfuck.o -o $1/build/brainfuck
 rm $1/build/brainfuck.o $1/temp.asm
